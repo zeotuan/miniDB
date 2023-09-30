ea6767f8e1a0b9335a6b10e8cbeef04cfda9d3b2
zeotuan <zeotuan@gmail.com>
2023-09-30 15:08:21 +1000

    FileHandle + BlockHandle Impl

--- Buffer/Handle.py
+++ Buffer/Handle.py
@@ -11,6 +11,40 @@ class BlockHandle:
         self._block_size = 300
         self._block_count = 0
         self.path = path
+        self.add_block(self._first_block)
+
+    @property
+    def block_count(self) -> int:
+        return self._block_count
+
+    @property
+    def usable_block(self) -> BlockInfo | None:
+        if self.block_count == 0:
+            return None
+        block = self._first_block.next
+        assert block is not None
+        self._first_block.next = block.next
+        self._block_count -= 1
+        block.reset_age()
+        block.next = None
+        return block
+
+    def add_block(self, block: BlockInfo) -> BlockInfo:
+        adder = BlockInfo(0)
+        adder.next = block.next
+        block.next = adder
+        self._block_count += 1
+        if self._block_count == self._block_size:
+            return adder
+        return self.add_block(adder)
+
+    def free_block(self, block: BlockInfo) -> None:
+        if self.block_count == 0:
+            self._first_block = block
+            block.next = block
+        else:
+            block.next = self._first_block.next
+            self._first_block.next = block
 
 
 class FileHandle:
@@ -72,24 +106,31 @@ class BlockInfo(object):
             file.readinto(self.data)
 
     def write_info(self, path: str) -> None:
-        pass
+        assert self.file is not None
+        path = os.path.join(path, self.file.file_path)
+        with open(path, "wb") as file:
+            file.seek(self.block_num * BLOCK_SIZE)
+            file.write(self.data)
 
 
 class FileInfo:
     def __init__(
         self,
-        db_name: str,
-        file_name: str,
-        file_type: int,
-        path: str,
-        record_num: int,
-        record_len: int,
-        first_block: BlockInfo,
-        next: FileInfo,
+        db_name: str = "",
+        file_name: str = "",
+        file_type: int = 0,
+        record_num: int = 0,
+        record_len: int = 0,
+        first_block: BlockInfo | None = None,
+        next_file: FileInfo | None = None,
     ) -> None:
         self.db_name = db_name
         self.file_type = file_type
         self.file_name = file_name
+        self._record_num = record_num
+        self._record_len = record_len
+        self._first_block = first_block
+        self._next_file = next_file
 
     @property
     def file_path(self) -> str:
@@ -98,3 +139,25 @@ class FileInfo:
     @property
     def extension(self) -> str:
         return ".index" if self.file_type == 1 else ".records"
+
+    @property
+    def first_block(self) -> BlockInfo | None:
+        return self._first_block
+
+    @first_block.setter
+    def first_block(self, block: BlockInfo | None) -> None:
+        self._first_block = block
+
+    @property
+    def next_file(self) -> FileInfo | None:
+        return self._next_file
+
+    @next_file.setter
+    def next_file(self, file: FileInfo | None) -> None:
+        self._next_file = file
+
+    def inc_record_num(self) -> None:
+        self._record_num += 1
+
+    def inc_record_len(self) -> None:
+        self._record_len += BLOCK_SIZE
